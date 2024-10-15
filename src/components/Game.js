f4109f824a0ea0abf8039a3f8a839086381463d0
Dylan <dylan.10293@gmail.com>
2024-10-15 19:30:45 -0400

    Added functionality to travel time in move history

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,21 +4,25 @@ import Board from './Board'
 const Game = () => {
 	const [xIsNext, setXIsNext] = useState(true);
 	const [history, setHistory] = useState([Array(9).fill(null)]);
-	const currentSquares = history[history.length - 1];
+	const [currentMove, setCurrentMove] = useState(0);
+
+	const currentSquares = history[currentMove];
 
 	const handlePlay = (nextSquares) => {
-		const nextHistory = history.slice(0, history.length);
-		setHistory([...nextHistory, nextSquares]);
+		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+		setHistory(nextHistory);
+		setCurrentMove(nextHistory.length - 1);
 		setXIsNext(!xIsNext);
 	}
-	const jumpTo = () => {
-		// TODO
+	const jumpTo = (nextMove) => {
+		setCurrentMove(nextMove);
+		setXIsNext(nextMove % 2 === 0);
 	}
 
 	const moves = history.map((squares, move) => {
 		let description = move > 0 ? 'Go to move #' + move : "Go to game start";
 		return (
-			<li>
+			<li key={move}>
 				<button onClick={() => jumpTo(move)}>{description}</button>
 			</li>
 		)
