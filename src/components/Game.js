734e5adae727a6361765c5b6620b10d0a7b895b8
Dylan <dylan.10293@gmail.com>
2024-10-15 19:17:21 -0400

    Shifted game logic from the Board component to the Game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Board from './Board'
 
 const Game = () => {
+	const [xIsNext, setXIsNext] = useState(true);
+	const [history, setHistory] = useState([Array(9).fill(null)]);
+	const currentSquares = history[history.length - 1];
+
+	const handlePlay = (nextSquares) => {
+		const nextHistory = history.slice(0, history.length);
+		setHistory([...nextHistory, nextSquares]);
+		setXIsNext(!xIsNext);
+	}
+
 	return (
 		<div className="game">
 			<div className="game-board">
-				<Board />
+				<Board
+					xIsNext={xIsNext}
+					squares={currentSquares}
+					onPlay={handlePlay}
+				/>
 			</div>
 			<div className="game-info">
 				<ol></ol>
